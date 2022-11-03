53db35994db8a5ac88ed60d7b4cb7574fc35e71a
HoangDuong1106 <maihoangduong11062000@gmail.com>
2022-11-03 10:12:08 +0700

    add schedulingVM

---
+++ CinemaSystem/ViewModel/SchedulingVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CinemaSystem.ViewModel
+{
+    public class SchedulingVM
+    {
+        public DateTime? Date { get; set; }
+        public int? CinemaId { get; set; }
+        public int? FilmId { get; set; }
+
+
+    }
+}
